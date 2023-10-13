8e085906eb48e105a313862773c4646f36561476
Icaro Paiva <icaro.paiva@acct.global>
2023-10-12 22:25:23 -0300

    feat: removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { PagesModule } from './pages/pages.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [ConfigModule.forRoot(), PagesModule],
 })
 export class AppModule {}
